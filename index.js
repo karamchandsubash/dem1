2a81193ce4acbecb3b65572f509c806d386fa5e0
KARAMCHAND SUBASH K <karamchandsubash@gmail.com>
2021-10-04 21:01:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 const PORT = process.env.PORT || 3000;
-http.createServer(function(req,res){
+function onRequest(req,res){
     res.writeHead(200,{'Content-type':'text/html'});
     fs.readFile('./index.html',null,function(error,data){
         if(error){
@@ -13,5 +13,6 @@ http.createServer(function(req,res){
         }
         res.end();
 
-    })
-})
+    });
+}
+http createServer(onRequest).listen();
